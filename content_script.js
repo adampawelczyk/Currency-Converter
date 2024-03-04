7689b52d1843b883e336202cbce513555d32357f
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2024-03-04 23:31:57 +0100

    Revert "Improved Error Message Handling"
    This reverts commit 2ddf9d6a5e4ada0fc5b19c4501cf95e2b14eded0.

--- content_script.js
+++ content_script.js
@@ -48,16 +48,9 @@ async function showCurrencyRate() {
         currencyRateDiv.innerHTML = `${formattedResult} ${currencyCode}`;
     } catch(error) {
         if (currencyRateDiv) {
-            let translatedMessage = await getTranslation(error.message);
+            const translatedMessage = await getTranslation(error.message);
             if (!translatedMessage) {
-                translatedMessage = await getTranslation('Error while translating error message');
-
-                if (!translatedMessage) {
-                    currencyRateDiv.innerHTML = error.message;
-                } else {
-                    currencyRateDiv.innerHTML = translatedMessage + ':</br>' + error.message;
-                }
-                
+                currencyRateDiv.innerHTML = await getTranslation('Error while translating error message') + ':</br>' + error.message;
             } else {
                 currencyRateDiv.innerHTML = translatedMessage;
             }
